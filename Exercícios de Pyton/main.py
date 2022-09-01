ad174c094198dfff4fd629297615d96ac2738cca
Lilian <liliandevsistem@gmail.com>
2022-09-01 10:24:16 -0400

    Criado para testar códigos

---
+++ Exercícios de Pyton/main.py
@@ -0,0 +1,7 @@
+login = imput(" Digiter seu Login: ")
+senha = imput("Digite sua senha: ")
+
+if login == "userpy" and senha == "teste123":
+  print("Bem vindo userpy01")
+  else:
+    print(" Login falhou, verifique usuário e senha e tente novamente")
